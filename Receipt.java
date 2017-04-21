a1fcfea6ab496bb037bc87f7cde7623ff8c6eaac
EvanjCollins <ecollins@iastate.edu>
2017-04-21 10:28:44 -0500

    Add files via upload

---
+++ Receipt.java
@@ -0,0 +1,132 @@
+import java.io.PrintWriter;
+import java.io.File;
+//import the decimal format to format the decimal numbers
+import java.io.FileNotFoundException;
+
+//util package to use the Scanner class
+
+import java.text.DecimalFormat;
+
+import java.util.*;
+
+//Store class
+
+public class Receipt
+
+{
+
+     //main method
+
+     public static void main(String args[]) throws FileNotFoundException
+
+     {
+    	 // PrinterWriter needs completed
+File file = new File ("Desktop/file.txt");
+PrintWriter printWriter = new PrintWriter ("file.txt"); 
+printWriter.println("Hello World!");
+
+          //Scanner object
+
+          Scanner in=new Scanner(System.in);
+
+          //define a string Decimal pattern
+
+          String pattern = "0000.00";
+
+          //create a DecimalFormat object
+
+          DecimalFormat decimalFormat = new DecimalFormat(pattern);
+
+          //declare the string array of items
+
+          String items[]=new String[]{"Single", "Double", "Queen", "King"};
+
+          //define and declare the itemquantity array
+
+          //and unitCost array
+
+          int itemquantity[]=new int[items.length];
+
+          double unitCost[]=new double[]{99,129,69,39};
+
+          //Prompt the user to enter the quantities
+
+          System.out.println("Enter number of nights stayed under the customers room type:");
+
+          //loop to get the quantity of each item
+
+          for(int i=0;i<items.length;i++)
+
+          {
+
+              System.out.println("Room type "+items[i]+": ");
+
+              itemquantity[i]=in.nextInt();
+
+          }
+
+          //compute the cost of each item
+
+          double cost[]=new double[unitCost.length];
+
+          for(int i=0;i<unitCost.length;i++)
+
+          {
+
+              cost[i]=unitCost[i]*itemquantity[i];
+
+          }
+
+          //declare the required variables
+
+          double total=0;
+
+          double HST=0;
+
+          double grandTotal=0;
+
+          //print the header of the format
+
+          String head[]={"Item","Qty","Unit Price","Total Price"};
+
+          //print the detailed format
+
+          String space=" ";
+
+          System.out.printf("%s %20s %15s %30s ", head[0], head[1], head[2], head[3]);
+
+          System.out.println("\n==========================================================================");
+
+          for(int i=0;i<items.length;i++)
+
+          {        
+
+              System.out.printf("%-13s %10d %15.2f %20s $%s\n",items[i], itemquantity[i], unitCost[i],space, decimalFormat.format(cost[i]));
+
+              total+=cost[i];
+
+          }
+
+          System.out.println("\n==========================================================================");
+
+          //compute the HST
+
+          HST=total*13/100;
+
+          //Compute the grandTotal
+
+          grandTotal=total+HST;
+
+          //print the final result
+
+          String subInfo[]={"Sub Total", "13% HST", "Grand Total"};
+
+          System.out.printf("%45s %15s $%s\n", subInfo[0],space, decimalFormat.format(total) );
+
+          System.out.printf("%45s %15s $%s\n", subInfo[1],space, decimalFormat.format(HST));
+
+          System.out.printf("%45s %15s $%s\n", subInfo[2],space, decimalFormat.format(grandTotal));
+
+     }
+
+}
